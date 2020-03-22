9b47591995e02934b430a7a437998844952b3101
Geoffrey Casper <gec8773@rit.edu>
2020-03-22 11:06:41 -0400

    Amended the @Run annotations' documentation

--- src/java/edu/rit/gec8773/laps/annotation/RunAfterEachInit.java
+++ src/java/edu/rit/gec8773/laps/annotation/RunAfterEachInit.java
@@ -5,7 +5,9 @@ import java.lang.annotation.*;
 
 /**
  * Indicates that the annotated method should be run after each instantiation
- * in that method's declaring class as an instance method
+ * in that method's declaring class as an instance method. Note that if more
+ * than one method is annotated, there is no guarantee which order the methods
+ * will run in.
  */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
--- src/java/edu/rit/gec8773/laps/annotation/RunBeforeFirstInit.java
+++ src/java/edu/rit/gec8773/laps/annotation/RunBeforeFirstInit.java
@@ -5,7 +5,9 @@ import java.lang.annotation.*;
 
 /**
  * Indicates that the annotated method should be run before the first
- * instantiation in that method's declaring class as a static method
+ * instantiation in that method's declaring class as a static method. Note that
+ * if more than one method is annotated, there is no guarantee which order the
+ * methods will run in.
  */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
