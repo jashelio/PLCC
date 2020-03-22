fb52daa7c07b3d30f61670a5178b869e37894f16
Geoffrey Casper <gec8773@rit.edu>
2020-03-22 11:14:18 -0400

    Added support for multiple @Run annotated methods

--- src/java/edu/rit/gec8773/laps/annotation/AnnotationUtils.java
+++ src/java/edu/rit/gec8773/laps/annotation/AnnotationUtils.java
@@ -4,6 +4,8 @@ import edu.rit.gec8773.laps.Consumer;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AnnotationUtils {
 
@@ -20,35 +22,41 @@ public class AnnotationUtils {
 		return token != null && token.skip();
 	}
 
-	public static Consumer<Void> getStaticMethod(Class<?> cls, // TODO add check for multiple methods
-												 Class<? extends Annotation> annotation) {
+	public static Consumer<Void> getStaticMethod(Class<?> cls,
+									 Class<? extends Annotation> annotation) {
+		List<Method> methods = new ArrayList<>();
 		for (Method method : cls.getMethods()) {
 			if (method.getAnnotation(annotation) == null)
 				continue;
 			int modifiers = method.getModifiers();
 			if (Modifier.isStatic(modifiers) && Modifier.isPublic(modifiers))
-				return ignore -> {
-					try {
-						method.invoke(null);
-					} catch (IllegalAccessException ignored) {}
-				};
+				methods.add(method);
 		}
-		return ignore -> {};
+		return ignore -> {
+			for (Method method : methods) {
+				try {
+					method.invoke(null);
+				} catch (IllegalAccessException ignored) {}
+			}
+		};
 	}
 
-	public static Consumer<Object> getInstanceMethod(Class<?> cls, // TODO add check for multiple methods
-													 Class<? extends Annotation> annotation) {
+	public static Consumer<Object> getInstanceMethod(Class<?> cls,
+									 Class<? extends Annotation> annotation) {
+		List<Method> methods = new ArrayList<>();
 		for (Method method : cls.getMethods()) {
 			if (method.getAnnotation(annotation) == null)
 				continue;
 			int modifiers = method.getModifiers();
 			if (!Modifier.isStatic(modifiers) && Modifier.isPublic(modifiers))
-				return object -> {
-					try {
-						method.invoke(object);
-					} catch (IllegalAccessException ignored) {}
-				};
+				methods.add(method);
 		}
-		return object -> {};
+		return object -> {
+			for (Method method : methods) {
+				try {
+					method.invoke(object);
+				} catch (IllegalAccessException ignored) {}
+			}
+		};
 	}
 }
