a560d457675d62feee08e4d8ab14296d4d80ace2
Geoffrey Casper <gec8773@rit.edu>
2020-03-23 11:52:55 -0400

    Added basic script templates to run LAPS in production

---
+++ src/bash/laps.sh
@@ -0,0 +1,3 @@
+LAPS_DIR=
+MAIN_CLASS=edu.rit.gec8773.laps.Main
+java -cp "$LAPS_DIR/LAPS.jar:$(pwd)" $MAIN_CLASS "$@"
---
+++ src/batch/laps.bat
@@ -0,0 +1,3 @@
+set LAPS_DIR=
+set MAIN_CLASS=edu.rit.gec8773.laps.Main
+java -cp "%LAPS_DIR%\LAPS.jar;%CD%" %MAIN_CLASS% %*
