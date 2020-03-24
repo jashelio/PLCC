63983a309d773a4cb2df75787c32af9328adadbf
Geoffrey Casper <gec8773@rit.edu>
2020-03-24 09:57:37 -0400

    Updated basic script templates

--- src/bash/laps.sh
+++ src/bash/laps.sh
@@ -1,3 +1,3 @@
-LAPS_DIR=
+LAPS_DIR="" # This is your LAPS installation directory (inside quotes)
 MAIN_CLASS=edu.rit.gec8773.laps.Main
 java -cp "$LAPS_DIR/LAPS.jar:$(pwd)" $MAIN_CLASS "$@"
--- src/batch/laps.bat
+++ src/batch/laps.bat
@@ -1,3 +1,3 @@
-set LAPS_DIR=
+set LAPS_DIR=C:\Users\gcasp\IdeaProjects\LAPS\out\jar & rem This is your LAPS installation directory
 set MAIN_CLASS=edu.rit.gec8773.laps.Main
 java -cp "%LAPS_DIR%\LAPS.jar;%CD%" %MAIN_CLASS% %*
